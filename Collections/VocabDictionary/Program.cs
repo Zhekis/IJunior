d535e3e896b43c71188a213db2dc4c837db1fdd1
Zhekis <Zheki4@gmail.com>
2022-10-30 01:00:26 +0500

    attempt 1

--- Collections/VocabDictionary/Program.cs
+++ Collections/VocabDictionary/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace VocabDictionary
+﻿using System;
+
+namespace VocabDictionary
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string searchWord = "";
+            string getWord = " ";
+            Dictionary <string, string> exlanatoryDict = new Dictionary<string, string> ()
+            {
+                { "Игра", "Один из видов активности человека и животных в процессе их жизнедеятельности."},
+                { "Смартфон", "Устройство, объединяющее в себе функции персонального органайзера и мобильного телефона."},
+                { "Шпонка", "Соединительный элемент, устанавливаемый в пазах двух деталей."}
+            };
+            Console.WriteLine("Введите слово :");
+            searchWord = Console.ReadLine();
+
+            foreach (var words in exlanatoryDict)
+            {
+                if (searchWord == words.Key)
+                    getWord = words.Key;
+            }
+
+            if (searchWord == getWord)
+                Console.WriteLine($"{searchWord} - {exlanatoryDict[getWord]}");
+            else
+                Console.WriteLine("Слово не найдено");
         }
     }
 }
