ef4c56e9b3f880f506600a3d4a746875bd159f78
Zhekis <Zheki4@gmail.com>
2022-11-11 23:32:06 +0500

    start

---
+++ OOP/Class Player/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Class_Player
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player hero = new Player("Zhek", 10, 100);
+
+            hero.ShowInfo();
+        }
+    }
+
+    class Player
+    {
+        public string Name;
+        public int Level;
+        public int Health;
+
+        public Player(string name, int level, int health)
+        {
+            Name = name;
+            Level = level;
+            Health = health;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name player : " + Name + "\nLevel : " + Level + "\nHealth : " + Health);
+        }
+    }
+}
