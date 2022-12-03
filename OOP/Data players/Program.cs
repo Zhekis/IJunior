ab075f55cbcdaa2d86e2fe6a1cb79bcc0cbfa6d4
Zhekis <Zheki4@gmail.com>
2022-12-04 02:05:01 +0500

    ver2

--- OOP/Data players/Program.cs
+++ OOP/Data players/Program.cs
@@ -11,105 +11,25 @@
 
     class Data
     {
-        const string ShowDataPlayers = "1";
-        const string AddNewPlayer = "2";
-        const string RemoveIdPlayer = "3";
-        const string BanIdPlayer = "4";
-        const string Exit = "5";
-        string userInput;
-        bool isWork = true;
-        int nextIdNumber = 111;
+        private const string ShowDataPlayers = "1";
+        private const string AddNewPlayer = "2";
+        private const string RemoveIdPlayer = "3";
+        private const string BanIdPlayer = "4";
+        private const string UnbanIdPlayer = "5";
+        private const string Exit = "6";
+        private int nextIdNumber = 111;
         private List<Player> _players = new List<Player>();
-
-        public Data(/*List<Player> players*/)
-        {
-            //_players = players;
-        }
-
-        public void ShowAllPlayers(int playersCount)
-        {
-            for(int i = 0; i < playersCount; i++)
-            {
-                _players[i].ShowInfo();
-            }
-        }
-
-        public int AddPlayer()
-        {
-            Console.WriteLine("What's name new player?");
-            string nameNewPlayer = Console.ReadLine();
-            Console.WriteLine("What's level new player?");
-            int levelNewPlayer = ReadInt();
-            _players.Add(new Player(nextIdNumber, nameNewPlayer, levelNewPlayer, false));
-            return nextIdNumber++;
-        }
-
-        public void DeletePlayer()
-        {
-            Console.WriteLine("What's ID delete?");
-            int idPlayer = ReadInt();
-
-            for (int i = 0; i < _players.Count; i++)
-            {
-                if (idPlayer == _players[i].GetId())
-                {
-                    Console.Write("Deleted : ");
-                    _players[i].ShowInfo();
-                    _players.RemoveAt(i);
-                }
-                else
-                {
-                    Console.WriteLine("Not find");
-                }
-            }
-        }
-
-        public void BanPlayer()
-        {
-            Console.WriteLine("What's ID ban?");
-            int idPlayer = ReadInt();
-
-            for (int i = 0; i < _players.Count; i++)
-            {
-                if (idPlayer == _players[i].GetId())
-                {
-                    Console.Write("Ban : ");
-                    _players[i].BanId();
-                    _players[i].ShowInfo();
-                }
-                else
-                {
-                    Console.WriteLine("Not find");
-                }
-            }
-        }
-
-        public int ReadInt()
-        {
-            bool isNumber = false;
-            int result = 0;
-
-            while (isNumber == false)
-            {
-                string userInput = Console.ReadLine();
-                isNumber = int.TryParse(userInput, out int number);
-
-                if (isNumber == true)
-                    result = number;
-                else
-                    Console.WriteLine("Введите число.");
-            }
-
-            return result;
-        }
 
         public void Work()
         {
-            while(isWork)
+            bool isWork = true;
+            string userInput;
+
+            while (isWork)
             {
                 Console.Clear();
                 Console.WriteLine("Что хотите сделать?");
-                Console.WriteLine(ShowDataPlayers + " - Открыть базу данных.\n" + AddNewPlayer + " - Добавить игрока.\n" + RemoveIdPlayer + " - Удалить игрока.\n" + BanIdPlayer + " - Бан по ID.\n" + Exit + " - Выход");
+                Console.WriteLine(ShowDataPlayers + " - Открыть базу данных.\n" + AddNewPlayer + " - Добавить игрока.\n" + RemoveIdPlayer + " - Удалить игрока.\n" + BanIdPlayer + " - Бан по ID.\n" + UnbanIdPlayer + " - Разбан по ID.\n" + Exit + " - Выход");
                 userInput = Console.ReadLine();
 
                 switch (userInput)
@@ -130,10 +50,104 @@
                         BanPlayer();
                         Console.ReadKey();
                         break;
+                    case UnbanIdPlayer:
+                        UnBanPlayer();
+                        Console.ReadKey();
+                        break;
                     case Exit:
                         isWork = false;
                         break;
                 }
+            }
+        }
+
+        private void ShowAllPlayers(int playersCount)
+        {
+            for(int i = 0; i < playersCount; i++)
+            {
+                _players[i].ShowInfo();
+            }
+        }
+
+        private int AddPlayer()
+        {
+            Console.WriteLine("What's name new player?");
+            string nameNewPlayer = Console.ReadLine();
+            Console.WriteLine("What's level new player?");
+            int levelNewPlayer = ReadInt();
+            _players.Add(new Player(nextIdNumber, nameNewPlayer, levelNewPlayer, false));
+            return nextIdNumber++;
+        }
+
+        private void DeletePlayer()
+        {
+            if (TryGetPlayer(out Player player) == true)
+            {
+                _players.Remove(player);
+                Console.WriteLine("Игрок успешно удалён!");
+            }
+        }
+
+        private void BanPlayer()
+        {
+            if (TryGetPlayer(out Player player) == true)
+            {
+                player.BanId();
+                Console.WriteLine("Игрок забанен!");
+            }
+        }
+
+        private void UnBanPlayer()
+        {
+            if (TryGetPlayer(out Player player) == true)
+            {
+                player.UnbanId();
+                Console.WriteLine("Игрок разбанен!");
+            }
+        }
+
+        private int ReadInt()
+        {
+            bool isNumber = false;
+            int result = 0;
+
+            while (isNumber == false)
+            {
+                string userInput = Console.ReadLine();
+                isNumber = int.TryParse(userInput, out int number);
+
+                if (isNumber == true)
+                    result = number;
+                else
+                    Console.WriteLine("Введите число.");
+            }
+
+            return result;
+        }
+
+        private bool TryGetPlayer(out Player player)
+        {
+            {
+                int idToFind;
+                bool isPlayer;
+                Console.WriteLine("Введите ID игрока.");
+                isPlayer = int.TryParse(Console.ReadLine(), out idToFind);
+
+                if (isPlayer)
+                {
+                    for (int i = 0; i < _players.Count; i++)
+                    {
+                        if (_players[i].Id == idToFind)
+                        {
+                            player = _players[i];
+                            return true;
+                        }
+                    }
+                }
+
+                Console.WriteLine("Такого игрока нет.");
+                player = null;
+                return false;
             }
         }
     }
@@ -158,10 +172,22 @@
             Console.WriteLine("Name player : " + _name + " Level : " + _level + " ID : " + _id + " Ban : " + _isBan);
         }
 
-        public int GetId()
-            { return _id; }
+        public int Id
+        { 
+            get 
+            { 
+                return _id; 
+            } 
+        }
 
         public void BanId()
-        { _isBan = true; }
+        { 
+            _isBan = true;
+        }
+
+        public void UnbanId()
+        {
+            _isBan = false;
+        }
     }
 }
