299bb6b0744706a8e3b0739640bff42a51047dfa
Zhekis <Zheki4@gmail.com>
2023-01-20 23:58:59 +0500

    fix fields del method

--- OOP/Store/Program.cs
+++ OOP/Store/Program.cs
@@ -60,11 +60,10 @@ namespace Store
 
             if (salesman.TryGetProduct(out Product product) == true)
             {
-                if (player.BalanceMoney >= product.Price)
+                if (player.BalanceMoneyPlayer >= product.Price)
                 {
-                    salesman.SellProduct(product);
                     receivedMoney = player.BuyProduct(product);
-                    salesman.TakeMoney(receivedMoney);
+                    salesman.SellProduct(product, receivedMoney);
                     Console.WriteLine($"Товар {product.Name} куплен");
                 }
                 else
@@ -79,39 +78,26 @@ namespace Store
 
     class Participant
     {
-        protected List<Product> _goods;
-        protected int _balanceMoney;
+        protected List<Product> Goods;
+        protected int BalanceMoney;
         public Participant(int balanceMoney, List<Product> goods)
         {
-            _goods = goods;
-            _balanceMoney = balanceMoney;
-        }
-
-        public void ShowItems()
-        {
-            Console.Clear();
-            Console.WriteLine($"Money: {_balanceMoney}");
-
-            for (int i = 0; i < _goods.Count; i++)
-            {
-                Console.WriteLine(_goods[i].Name);
-            }
-
-            Console.ReadKey();
+            Goods = goods;
+            BalanceMoney = balanceMoney;
         }
 
         public void ShowGoods()
         {
             Console.Clear();
-            Console.WriteLine($"Money: {_balanceMoney}");
 
-            for (int i = 0; i < _goods.Count; i++)
+            for (int i = 0; i < Goods.Count; i++)
             {
                 Console.WriteLine($"Number {i + 1}");
-                _goods[i].ShowInfo();
+                Goods[i].ShowInfo();
             }
 
             Console.ReadKey();
+            Console.WriteLine();
         }
     }
 
@@ -119,41 +105,34 @@ namespace Store
     {
         public Salesman(int balanceMoney, List<Product> goods) : base(balanceMoney, goods)
         {
-            _goods.Add(new Product("Apple", 1));
-            _goods.Add(new Product("Knife", 3));
-            _goods.Add(new Product("Phone", 5));
-            _goods.Add(new Product("Map", 2));
+            Goods.Add(new Product("Apple", 1));
+            Goods.Add(new Product("Knife", 3));
+            Goods.Add(new Product("Phone", 5));
+            Goods.Add(new Product("Map", 2));
         }
 
-        public void SellProduct(Product soldProduct)
+        public void SellProduct(Product soldProduct, int gettingMoney)
         {
-            _goods.Remove(soldProduct);
-        }
-
-        public void TakeMoney(int gettingMoney)
-        {
-            _balanceMoney += gettingMoney;
-            Console.WriteLine($"Баланс продавца: {_balanceMoney}");
+            Goods.Remove(soldProduct);
+            BalanceMoney += gettingMoney;
         }
 
         public bool TryGetProduct(out Product product)
         {
+            int numberToFind;
+            Console.WriteLine("Введите номер товара для покупки.");
+            bool isNumber = int.TryParse(Console.ReadLine(), out numberToFind);
+
+            if (numberToFind > 0 && numberToFind <= Goods.Count)
             {
-                int numberToFind;
-                Console.WriteLine("Введите номер товара для покупки.");
-                bool isNumber = int.TryParse(Console.ReadLine(), out numberToFind);
-
-                if (numberToFind > 0 && numberToFind <= _goods.Count)
-                {
-                    numberToFind--;
-                    product = _goods[numberToFind];
-                    return true;
-                }
-
-                Console.WriteLine("Такого товара нет.");
-                product = null;
-                return false;
+                numberToFind--;
+                product = Goods[numberToFind];
+                return true;
             }
+
+            Console.WriteLine("Такого товара нет.");
+            product = null;
+            return false;
         }
     }
 
@@ -161,23 +140,17 @@ namespace Store
     {
         public Player(int balanceMoney, List<Product> items) : base(balanceMoney, items)
         {
-            _goods.Add(new Product("Keys", 2));
-            _goods.Add(new Product("Glasses", 3));
+            Goods.Add(new Product("Keys", 2));
+            Goods.Add(new Product("Glasses", 3));
         }
 
-        public int BalanceMoney
-        {
-            get
-            {
-                return _balanceMoney;
-            }
-        }
+        public int BalanceMoneyPlayer => BalanceMoney;
 
         public int BuyProduct(Product boughtGood)
         {
-            _goods.Add(boughtGood);
-            _balanceMoney -= boughtGood.Price;
-            Console.WriteLine($"У вас осталось {_balanceMoney}");
+            Goods.Add(boughtGood);
+            BalanceMoney -= boughtGood.Price;
+            Console.WriteLine($"У вас осталось денег: {BalanceMoney}");
             return boughtGood.Price;
         }
     }
