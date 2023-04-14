5bfb497e33647d756d8fec0cdcf93f72affda143
zh <zh@DESKTOP-D708L2T>
2023-04-14 14:56:44 +0500

    Zoo start

---
+++ OOP/Zoo/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Zoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        class ZooApplication
+        {
+            private Zoo _zoo;
+        }
+
+        class Zoo
+        {
+            private List<Aviary> _aviaryes;
+        }
+
+        class Aviary
+        {
+            private List<Animal> _animals;
+        }
+
+        class Animal
+        {
+
+        }
+
+    }
+}
