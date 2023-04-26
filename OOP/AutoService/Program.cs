00aa3a6a9a7b47a06a8f7a45d831109876350c23
zh <zh@DESKTOP-D708L2T>
2023-04-26 09:18:39 +0500

    autoService attempt two

--- OOP/AutoService/Program.cs
+++ OOP/AutoService/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.Metrics;
 using System.Runtime.ConstrainedExecution;
+using System.Xml.Linq;
 
 namespace AutoService
 {
@@ -18,19 +19,12 @@ namespace AutoService
     {
         private int _balanceMoney = 100000;
         private int _fine = 5000;
-        private List<Detail> _details;
-        private Dictionary<string, int> _countParts = new Dictionary<string, int>();
+        private Stock _stock = new Stock();
         private Queue<Car> _cars = new Queue<Car>();
 
         public Service()
         {
             Random random = new Random();
-            _details = new List<Detail>() {new Detail("Колесный диск", 5000, 200), new Detail("Шина", 4000, 200), new Detail("Радиатор", 8000, 2000), 
-            new Detail("Катализатор", 10000, 5000), new Detail("Масло", 5000, 1000), new Detail("Воздушный фильтр", 1000, 500),
-            new Detail("Шарнир колеса", 7000, 3000), new Detail("Прокладка ГБЦ", 1000, 3000), new Detail("Подушка безопасности", 4000, 2000), new Detail("Рычаг подвески", 3000, 1000),
-            new Detail("Сайлентблоки", 5000, 2000), new Detail("Пружины", 7000, 3000), new Detail("Стойка стабилизатора", 1000, 1000), new Detail("Тормозные колодки", 3000, 1000),
-            new Detail("Лобовое стекло", 15000, 2000), new Detail("Подшипник ступицы", 7000, 2000),};
-            CreateStock(random);
             CreateCars(30, random);
         }
 
@@ -42,24 +36,16 @@ namespace AutoService
                 Console.WriteLine($"Balance: {_balanceMoney}.В очереди {_cars.Count} авто.");
                 Console.WriteLine();
                 Console.WriteLine("Stock.");
-                ShowStock();
+                _stock.Show();
                 Console.WriteLine();
                 Car car = _cars.Dequeue();
                 Console.WriteLine($"Авто на обслуживании. После диагностики детали к замене:");
                 car.ShowBrokenDetails();
-                Console.WriteLine($"Total price: {GetTotalPriceRepairCar(car.BrokenDetails)}");
-                ServiceAndShowResult(car.BrokenDetails);
+                Console.WriteLine($"Total price: {car.GetTotalPriceRepair()}");
+                Serve(car);
                 Console.WriteLine();
                 Console.WriteLine("Нажмите для обслуживания следующего авто.");
                 Console.ReadKey();
-            }
-        }
-
-        public void ShowStock()
-        {
-            foreach (var detail in _countParts)
-            {
-                Console.WriteLine($"Detail: {detail.Key}  value: {detail.Value}");
             }
         }
 
@@ -67,48 +53,33 @@ namespace AutoService
         {
             int maxCountBrokenDetails = 5;
             int minCountBrokenDetails = 1;
+            int randomCountDetails = random.Next(minCountBrokenDetails, maxCountBrokenDetails);
+            int randomIndex;
             List<Detail> brokenDetails;
+            Detail detail;
 
             for (int i = 0; i < count; i++)
             {
                 brokenDetails = new List<Detail>();
 
-                for (int j = 0; j < random.Next(minCountBrokenDetails, maxCountBrokenDetails); j++)
+                for (int j = 0; j < randomCountDetails; j++)
                 {
-                    brokenDetails.Add(_details[random.Next(_details.Count)]);
+                    randomIndex = random.Next(_stock.CountNamesDetails);
+                    detail = new Detail(_stock.GetNameDetail(randomIndex), _stock.GetPriceDetail(randomIndex), _stock.GetPriceRepairDetail(randomIndex));
+                    brokenDetails.Add(detail);
                 }
 
                _cars.Enqueue(new Car(brokenDetails));
             }
         }
 
-        private void CreateStock(Random random)
+        private void Serve(Car car)
         {
-            int maxCountDetail = 10;
-
-            for (int i = 0; i < _details.Count; i++)
-            {
-                _countParts.Add(_details[i].Name, random.Next(maxCountDetail));
-            }
-        }
-
-        private int GetTotalPriceRepairCar(List<Detail> brokenDetails)
-        {
-            int result = 0;
-
-            for (int i = 0; i < brokenDetails.Count; i++)
-                result = brokenDetails[i].Price + brokenDetails[i].RepairPrice;
-
-            return result;
-        }
-
-        private void ServiceAndShowResult(List<Detail> brokenDetails)
-        {
-            if (IsEnoughDetailsInStock(brokenDetails))
+            if (IsEnoughDetailsInStock(car))
             {
                 Console.WriteLine("Repair completed successfully");
-                ChangePartAvailability(brokenDetails);
-                _balanceMoney += GetTotalPriceRepairCar(brokenDetails);
+                RemovePartsFromStock(car);
+                _balanceMoney += car.GetTotalPriceRepair();
             }
             else
             {
@@ -117,54 +88,168 @@ namespace AutoService
             }
         }
 
-        private bool IsEnoughDetailsInStock(List<Detail> brokenDetails)
+        private bool IsEnoughDetailsInStock(Car car)
         {
+            string name;
             int counter = 0;
             bool result = false;
 
-            for (int i = 0; i < brokenDetails.Count; i++)
+            for (int i = 0; i < car.CountBrokenDetails; i++)
             {
-                if (_countParts[brokenDetails[i].Name] > 0)
+                name = car.GetNameDetail(i);
+
+                if (_stock.IsAvailableDetail(name))
                     counter++;
             }
 
-            if (counter == brokenDetails.Count)
+            if (counter == car.CountBrokenDetails)
                 result = true;
 
             return result;
         }
 
-        private void ChangePartAvailability(List<Detail> brokenDetails)
+        private void RemovePartsFromStock(Car car)
         {
-            for (int i = 0; i < brokenDetails.Count; i++)
+            string name;
+
+            for (int i = 0; i < car.CountBrokenDetails; i++)
             {
-                _countParts[brokenDetails[i].Name]--;
+                name = car.GetNameDetail(i);
+                _stock.RemoveDetailFromCell(name);
             }
         }
     }
 
     class Car
     {
+        private List<Detail> _brokenDetails;
+
         public Car (List<Detail> brokenDetails)
         {
-            BrokenDetails = brokenDetails;
+            _brokenDetails = brokenDetails;
         }
 
-        public List<Detail> BrokenDetails { get; private set; }
+        public int CountBrokenDetails => _brokenDetails.Count;
+
+        public string GetNameDetail(int index)
+        {
+            return _brokenDetails[index].Name;
+        }
 
         public void ShowBrokenDetails()
         {
             int indexAddition = 1;
 
-            for (int i = 0; i < BrokenDetails.Count; i++)
+            for (int i = 0; i < _brokenDetails.Count; i++)
             {
                 Console.Write($"{i + indexAddition}. ");
-                Console.WriteLine(BrokenDetails[i].GetInfo());
+                Console.WriteLine(_brokenDetails[i].GetInfo());
+            }
+        }
+
+        public int GetTotalPriceRepair()
+        {
+            int result = 0;
+
+            for (int i = 0; i < _brokenDetails.Count; i++)
+                result += _brokenDetails[i].Price + _brokenDetails[i].RepairPrice;
+
+            return result;
+        }
+    }
+
+    class Stock
+    {
+        private List<Detail> _details;
+        private List<Cell> _cells = new List<Cell>();
+
+        public Stock()
+        {
+            Random random = new Random();
+            _details = new List<Detail>() {new Detail("Колесный диск", 5000, 200), new Detail("Шина", 4000, 200), new Detail("Радиатор", 8000, 2000),
+            new Detail("Катализатор", 10000, 5000), new Detail("Масло", 5000, 1000), new Detail("Воздушный фильтр", 1000, 500),
+            new Detail("Шарнир колеса", 7000, 3000), new Detail("Прокладка ГБЦ", 1000, 3000), new Detail("Подушка безопасности", 4000, 2000), new Detail("Рычаг подвески", 3000, 1000),
+            new Detail("Сайлентблоки", 5000, 2000), new Detail("Пружины", 7000, 3000), new Detail("Стойка стабилизатора", 1000, 1000), new Detail("Тормозные колодки", 3000, 1000),
+            new Detail("Лобовое стекло", 15000, 2000), new Detail("Подшипник ступицы", 7000, 2000),};
+            Create(random);
+        }
+
+        public int CountNamesDetails => _details.Count;
+
+        private void Create(Random random)
+        {
+            int maxCountDetail = 10;
+
+            for (int i = 0; i < _details.Count; i++)
+            {
+                _cells.Add(new Cell(_details[i], random.Next(maxCountDetail)));
+            }
+        }
+
+        public string GetNameDetail(int index)
+        {
+            return _details[index].Name;
+        }
+
+        public int GetPriceDetail(int index)
+        {
+            return _details[index].Price;
+        }
+
+        public int GetPriceRepairDetail(int index)
+        {
+            return _details[index].RepairPrice;
+        }
+
+        public bool IsAvailableDetail(string name)
+        {
+            foreach (var cell in _cells)
+            {
+                if (cell.Name == name && cell.Count > 0)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public void RemoveDetailFromCell(string name)
+        {
+            foreach (var cell in _cells)
+            {
+                if (cell.Name == name)
+                    cell.Remove();
+            }
+        }
+
+        public void Show()
+        {
+            foreach (var cell in _cells)
+            {
+                Console.WriteLine($"Detail: {cell.Name}  value: {cell.Count}");
             }
         }
     }
 
-    public struct Detail
+    class Cell
+    {
+        private Detail _detail;
+
+        public Cell(Detail detail, int count)
+        {
+            _detail = detail;
+            Count = count;
+        }
+
+        public string Name => _detail.Name;
+        public int Count { get; private set; }
+
+        public void Remove()
+        {
+            Count--;
+        }
+    }
+
+    class Detail
     {
         public Detail(string name, int price, int repairPrice)
         {
