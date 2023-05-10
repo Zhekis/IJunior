fafd69d0ec3194c4766856de328f0d3ffbadf1c5
zh <zh@DESKTOP-D708L2T>
2023-05-10 11:44:48 +0500

    TopPlayers attempt 1

---
+++ LINQ/TopPlayers/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Numerics;
+
+namespace TopPlayers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Player> players = new List<Player> {
+            new Player("RobertPupkin", 1, 100),
+            new Player("LarisaDolina", 2, 150),
+            new Player("BorisBritva", 1, 120),
+            new Player("AndreyMorj", 3, 180),
+            new Player("SamPopov", 3, 190),
+            new Player("GorgWashington", 3, 200),
+            new Player("PetrSidorov", 4, 300),
+            new Player("VasiliyPervii", 4, 350),
+            new Player("IvanGraga", 6, 400),
+            new Player("MelisaKirova", 5, 450)};
+
+            Terminal terminal = new Terminal();
+            var filteredPlayers = players.OrderByDescending(player => player.Level).Take(3);
+            Console.WriteLine("TOP3 LEVEL:");
+            terminal.ShowPlayers(filteredPlayers);
+            Console.WriteLine();
+            Console.WriteLine("TOP3 POWER:");
+            filteredPlayers = players.OrderByDescending(player => player.Power).Take(3);
+            terminal.ShowPlayers(filteredPlayers);
+        }
+    }
+
+    class Terminal
+    {
+        public void ShowPlayers(IEnumerable<Player> players)
+        {
+            foreach (var player in players)
+            {
+                player.ShowInfo();
+            }
+        }
+    }
+
+    class Player
+    {
+        public Player(string fullName, int level, int power)
+        {
+            FullName = fullName;
+            Level = level;
+            Power = power;
+        }
+
+        public string FullName { get; private set; }
+        public int Level { get; private set; }
+        public int Power { get; private set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{FullName}, Level: {Level}, Power: {Power}");
+        }
+    }
+}
